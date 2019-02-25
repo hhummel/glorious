e5a1bb13d49d88f321df22b2bacae4d0058e9a64
Ubuntu <ubuntu@ip-172-31-25-116.ec2.internal>
2019-02-25 13:27:38 +0000

    Working on table shading

--- static/scripts/formatOrders.js
+++ static/scripts/formatOrders.js
@@ -5,6 +5,6 @@ let previous = '';
 let marker = false;
 for (let j=0; j<elements.length; j++) {
   if (elements[j].textContent != previous && previous) marker = !marker;
-  if (marker) elements[j].parentNode.className="bg-secondary text-light";
+  if (marker) elements[j].parentNode.className="table-danger";
   previous = elements[j].textContent; 
 }
