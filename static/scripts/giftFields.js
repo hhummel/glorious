ebceac3e8469ddb8380b1bb01224848a499a1751
Ubuntu <ubuntu@ip-172-31-25-116.ec2.internal>
2019-02-28 01:14:09 +0000

    Fix giftFields.js

--- static/scripts/giftFields.js
+++ static/scripts/giftFields.js
@@ -1,8 +1,9 @@
 //Fields in order forms that pertain to gifts only appear if the order is a gift
 
-function showFields(element) {
+function makeVisible(element) {
   var checked = document.getElementById("id_this_is_a_gift").checked;
-  var el = document.getElementById('id_recipient_' + element);
+  var id = "id_recipient_" + element;
+  var el = document.getElementById(id);
   if (el) {
     if (checked) {
       el.className="form-control";
@@ -15,21 +16,23 @@ function showFields(element) {
       label.htmlFor="id_recipient_" + element;
       label.innerHTML="Recipient " + element;
       label.id="id_recipient_" + element + "_label";
-      el.parentElement.insertBefore(label, el);
-    } else {
+      el.parentElement.insertBefore(label, el);	    
+    }
+    else {
+      console.log("Found element and unchecked");
       el.type="hidden";
       var label=document.getElementById("id_recipient_" + element + "_label");
-      label.parentElement.removeChild(label);
+      label.parentElement.removeChild(label);	    
     }
   }
 }
 
 gift=document.getElementById("id_this_is_a_gift");
-gift.addEventListener('click', function() {
-  showFields("name");
-  showFields("address");
-  showFields("city");
-  showFields("state");
-  showFields("message");
-}, false)
+gift.addEventListener('click', function() {	
+  makeVisible("name");
+  makeVisible("address");
+  makeVisible("city");
+  makeVisible("state");
+  makeVisible("message");
+}, false);
 
