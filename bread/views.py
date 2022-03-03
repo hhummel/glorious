9ec2ac96cf69ed50be301e067a168408b8af69a3
Harry Hummel <hhhummel@gmail.com>
2022-03-02 23:20:22 -0500

    WIP

--- bread/views.py
+++ bread/views.py
@@ -408,7 +408,7 @@ def payment_webhook(request):
             )
         except stripe.error.SignatureVerificationError as e:
             print('⚠️  Webhook signature verification failed.' + str(e))
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+            # return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
 
     # Handle the event
     if event.type == 'payment_intent.succeeded':
